3c8c1b7549c54776369c623f21538046b28eef17
Joe Revans <hello@joerevans.com>
2021-11-26 19:50:58 +0100

    all changes expect for scene

---
+++ unity-wilder-models/Assets/Scripts/SaveLoadMenu.cs
@@ -0,0 +1,133 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SaveLoadMenu : MonoBehaviour
+{
+    public HexGrid hexGrid;
+    public Text menuLabel, actionButtonLabel;
+    public InputField nameInput;
+	public RectTransform listContent;
+	public SaveLoadItem itemPrefab;
+    bool saveMode;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+	public void Open (bool saveMode) {
+        this.saveMode = saveMode;
+		if (saveMode) {
+			menuLabel.text = "Save Map";
+			actionButtonLabel.text = "Save";
+		}
+		else {
+			menuLabel.text = "Load Map";
+			actionButtonLabel.text = "Load";
+		}
+        FillList();
+		gameObject.SetActive(true);
+		HexMapCamera.Locked = true;
+	}
+
+	public void Close () {
+		gameObject.SetActive(false);
+		HexMapCamera.Locked = false;
+	}
+	string GetSelectedPath () {
+        /*
+        InputField is locked to alphanumeric content to prevent entry of invalid chars
+        */
+		string mapName = nameInput.text;
+		if (mapName.Length == 0) {
+			return null;
+		}
+		return Path.Combine(Application.persistentDataPath, mapName + ".map");
+	}
+	void Save (string path) {
+        
+		using (
+			BinaryWriter writer =
+			new BinaryWriter(File.Open(path, FileMode.Create))
+		) {
+			writer.Write(1);
+			hexGrid.Save(writer);
+		}
+	}
+	void Load (string path) {
+
+		if (!File.Exists(path)) {
+			Debug.LogError("File does not exist " + path);
+			return;
+		}
+
+		using (BinaryReader reader = new BinaryReader(File.OpenRead(path))) {
+			int header = reader.ReadInt32();
+			if (header <= 1) {
+				hexGrid.Load(reader, header);
+				HexMapCamera.ValidatePosition();
+			}
+			else {
+				Debug.LogWarning("Unknown map format " + header);
+			}
+		}
+	}
+	public void Action () {
+		string path = GetSelectedPath();
+		if (path == null) {
+			return;
+		}
+		if (saveMode) {
+            Debug.Log("SAVE: " + path);
+			Save(path);
+		}
+		else {
+            Debug.Log("LOAD: " + path);
+			Load(path);
+		}
+		Close();
+	}
+	public void Delete () {
+		string path = GetSelectedPath();
+		if (path == null) {
+			return;
+		}
+		if (File.Exists(path)) {
+			File.Delete(path);
+		}
+        nameInput.text = "";
+		FillList();
+	}
+	public void SelectItem (string name) {
+		nameInput.text = name;
+	}
+	void FillList () {
+
+		for (int i = 0; i < listContent.childCount; i++) {
+			Destroy(listContent.GetChild(i).gameObject);
+		}
+
+		string[] paths =
+			Directory.GetFiles(Application.persistentDataPath, "*.map");
+
+        Array.Sort(paths);
+
+		for (int i = 0; i < paths.Length; i++) {
+			SaveLoadItem item = Instantiate(itemPrefab);
+			item.menu = this;
+			item.MapName = Path.GetFileNameWithoutExtension(paths[i]);
+			item.transform.SetParent(listContent, false);
+		}
+
+	}
+}
